bd5c767674370c69d680b32e76600325433a3874
Freddy Juhel <domaine75@gmail.com>
2014-11-13 09:45:28 +0100

    add mileage to elevator class

--- ElevatorDemo/Elevator.cs
+++ ElevatorDemo/Elevator.cs
@@ -8,8 +8,9 @@
     public int NumberPersonCarried { get; set; }
     public bool IsFull { get; set; }
     public int Position { get; set; }
-    public ElevatorMovingDirection MovingDirection { get; set; }  
-
+    public ElevatorMovingDirection MovingDirection { get; set; }
+    public decimal Mileage { get; set; }
+    
 
     public Elevator(int personCapacity = 1, bool isWorking = true, bool isMoving = false, int numberOfPersonCarried = 0, bool isFull = false, 
       int position = 0, ElevatorMovingDirection movingDirection = ElevatorMovingDirection.NotMoving)
@@ -21,6 +22,7 @@
       IsFull = isFull;
       Position = position; // what floor the elevator is at?
       MovingDirection = movingDirection;
+      Mileage = 0;
     }
 
   }
